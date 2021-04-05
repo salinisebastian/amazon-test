efddadc8d7d0b1a2974a38bf3bb14c85cdd71bc2
salinisebastian <salini.seb@gmail.com>
2021-04-05 20:42:55 +0200

    Adding cost of items

--- cypress/integration/login.spec.js
+++ cypress/integration/login.spec.js
@@ -7,7 +7,7 @@ describe("Our first suite", () => {
     cy.contains("Hallo, Anmelden").click();
     cy.get('[id="ap_email"]').type("paulissacnew2000@gmail.com");
     cy.get('[aria-labelledby="continue-announce"]').click();
-    cy.get('[id="ap_password"]').type("****");
+    cy.get('[id="ap_password"]').type("*****");
     cy.get('[id="signInSubmit"]').click();
   });
 
@@ -19,6 +19,34 @@ describe("Our first suite", () => {
     cy.selectProduct("laptop", "Intel");
     cy.wait(2000);
     cy.selectProduct("mobilephone", "Nokia");
+  });
+
+  it.only("Printing the cost of items", () => {
+    var sum = 0;
+
+    cy.get(".nav-cart-icon").click({ force: true });
+
+    cy.get("#activeCartViewForm")
+      .find(
+        ".a-size-medium.a-color-base.sc-price.sc-white-space-nowrap.sc-product-price.a-text-bold"
+      )
+      .each(($el, index, $list) => {
+        const cost = $el.text();
+        var result = cost.split(/(\s+)/);
+        result = result[0].trim().replace(",", ".");
+        sum = Number(sum) + Number(result);
+      })
+      .then(function () {
+        cy.log(sum);
+      });
+    cy.get(
+      '[class="a-size-medium a-color-base sc-price sc-white-space-nowrap"]'
+    ).then((total) => {
+      const cost = total.text();
+      var actualResult = cost.split(/(\s+)/);
+      actualResult = actualResult[0].trim().replace(",", ".");
+      expect(Number(sum.toFixed(2))).to.equal(Number(actualResult));
+    });
   });
 
   it("Clearing the cart if any items are present", () => {
